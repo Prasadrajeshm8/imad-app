e8c3f847b10be5d8068b16ed471fa4e1e3a292e0
Prasadrajeshm8 <30652296+Prasadrajeshm8@users.noreply.github.com>
2017-08-14 15:06:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element=document.getElementById('main.text');
 element.innerHTML="hi Rj";
+var img=document.getElementById('img'){
+    img.onclick=function(){
+        img.style.marginLeft='100px';
+    }
+    
+}
